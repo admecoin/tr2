3b97a65fd5450caf9fc4da333a0f40c9ee1781b0
admecoin <34143043+admecoin@users.noreply.github.com>
2018-11-12 21:19:17 +0200

    3

--- client/component/Card/CardAddressTXs.jsx
+++ client/component/Card/CardAddressTXs.jsx
@@ -55,7 +55,7 @@ export default class CardAddressTXs extends Component {
             amount: (
               <span
                 className={ `badge badge-${ isSpent ? 'danger' : 'success' }` }>
-                { numeral(amount).format('0,0.0000') } SAP
+                { numeral(amount).format('0,0.0000') } TIM
               </span>
             ),
             createdAt: dateFormat(tx.createdAt),
--- client/component/Card/CardEarnings.jsx
+++ client/component/Card/CardEarnings.jsx
@@ -23,7 +23,7 @@ const CardEarnings = ({ coin }) => {
           DAILY
         </div>
         <div className="col-sm-12 col-md-9">
-          { nbtc(day) } SAP / { nbtc(day * coin.btc) } BTC / { nusd(day * coin.usd) } USD
+          { nbtc(day) } TIM / { nbtc(day * coin.btc) } BTC / { nusd(day * coin.usd) } USD
         </div>
       </div>
       <div className="row">
@@ -31,7 +31,7 @@ const CardEarnings = ({ coin }) => {
           WEEKLY
         </div>
         <div className="col-sm-12 col-md-9">
-          { nbtc(week) } SAP / { nbtc(week * coin.btc) } BTC / { nusd(week * coin.usd) } USD
+          { nbtc(week) } TIM / { nbtc(week * coin.btc) } BTC / { nusd(week * coin.usd) } USD
         </div>
       </div>
       <div className="row">
@@ -39,7 +39,7 @@ const CardEarnings = ({ coin }) => {
           MONTHLY
         </div>
         <div className="col-sm-12 col-md-9">
-          { nbtc(month) } SAP / { nbtc(month * coin.btc) } BTC / { nusd(month * coin.usd) } USD
+          { nbtc(month) } TIM / { nbtc(month * coin.btc) } BTC / { nusd(month * coin.usd) } USD
         </div>
       </div>
       <div className="row">
@@ -47,7 +47,7 @@ const CardEarnings = ({ coin }) => {
           YEARLY
         </div>
         <div className="col-sm-12 col-md-9">
-          { nbtc(year) } SAP / { nbtc(year * coin.btc) } BTC / { nusd(year * coin.usd) } USD
+          { nbtc(year) } TIM / { nbtc(year * coin.btc) } BTC / { nusd(year * coin.usd) } USD
         </div>
       </div>
       <div className="row">
